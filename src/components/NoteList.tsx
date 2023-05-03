cbf11b51f6c812bc271192b7d556ede1030bd369
Jens Lauritsen <evertonstyrer@gmail.com>
2023-05-03 12:53:34 +0200

    delete funktion

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,16 +1,22 @@
-// NoteList.tsx
 import React from "react";
 
 interface NoteListProps {
     notes: { exercise: string; weight: number; reps: number; date: Date }[];
+    onDelete: (note: { exercise: string; weight: number; reps: number; date: Date }) => Promise<void>;
 }
 
-const NoteList: React.FC<NoteListProps> = ({ notes }) => {
+
+const NoteList: React.FC<NoteListProps> = ({ notes, onDelete }) => {
+    const handleDeleteNote = (note: { exercise: string; weight: number; reps: number; date: Date }) => {
+        onDelete(note);
+    };
+
     return (
         <ul>
             {notes.map((note, index) => (
                 <li className="bg-white" key={index}>
                     {note.exercise}, {note.weight} kg, {note.reps} reps, {note.date.toLocaleDateString()}
+                    <button onClick={() => handleDeleteNote(note)}>Delete</button>
                 </li>
             ))}
         </ul>
--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import NoteForm from "../components/NoteForm";
 import NoteList from "../components/NoteList";
-import { collection, addDoc, onSnapshot, query, orderBy, QuerySnapshot } from "firebase/firestore";
+import { collection, query, where, getDocs, deleteDoc, DocumentReference, Query, orderBy, DocumentData, QuerySnapshot, onSnapshot, addDoc, } from "firebase/firestore";
 import ProgressChart from "../components/ProgessChart";
 import firestore from "../firebase";
 
@@ -9,51 +9,74 @@ import firestore from "../firebase";
 
 // Definerer en React-komponent kaldet NotePage med typesikring
 const NotePage: React.FC = () => {
-    // Opretter en lokal tilstandsvariabel (notes) og en tilhørende opdateringsfunktion (setNotes)
-    // til at holde styr på noterne, som er en liste af objekter med exercise, weight og reps
     const [notes, setNotes] = useState<
-        { exercise: string; weight: number; reps: number; date: Date }[]
+      { exercise: string; weight: number; reps: number; date: Date }[]
     >([]);
     const [selectedExercise, setSelectedExercise] = useState<string>("");
-
+  
+    const notesCollection = collection(firestore, "notes");
+    const notesQuery = query(notesCollection, orderBy("date"));
+  
     useEffect(() => {
-        const notesCollection = collection(firestore, "notes");
-        const notesQuery = query(notesCollection, orderBy("date"));
-        const unsubscribe = onSnapshot(notesQuery, (snapshot: QuerySnapshot) => {
-            const newNotes = snapshot.docs.map((doc) => {
-                const noteData = doc.data();
-                return {
-                    exercise: noteData.exercise,
-                    weight: noteData.weight,
-                    reps: noteData.reps,
-                    date: noteData.date.toDate()
-                };
-            });
-            setNotes(newNotes);
+      const unsubscribe = onSnapshot(notesQuery, (snapshot: QuerySnapshot) => {
+        const newNotes = snapshot.docs.map((doc) => {
+          const noteData = doc.data();
+          return {
+            exercise: noteData.exercise,
+            weight: noteData.weight,
+            reps: noteData.reps,
+            date: noteData.date.toDate(),
+          };
         });
-
-        return () => {
-            unsubscribe();
-        };
+        setNotes(newNotes);
+      });
+  
+      return () => {
+        unsubscribe();
+      };
     }, []);
-
-    // Definerer en funktion (addNote), der tager en note og tilføjer den til notes-tilstanden
-    const addNote = async (note: { exercise: string; weight: number; reps: number; date: Date }) => {
-        const notesCollection = collection(firestore, "notes");
-        await addDoc(notesCollection, note);
+  
+    const addNote = async (note: {
+      exercise: string;
+      weight: number;
+      reps: number;
+      date: Date;
+    }) => {
+      await addDoc(notesCollection, note);
     };
-
-    // Returnerer JSX med en div, der indeholder NoteForm og NoteList komponenterne
+  
+    const deleteNote = async (note: {
+        exercise: string;
+        weight: number;
+        reps: number;
+        date: Date;
+      }) => {
+        const noteQuery: Query<DocumentData> = query(
+          notesCollection,
+          where("exercise", "==", note.exercise),
+          where("weight", "==", note.weight),
+          where("reps", "==", note.reps),
+          where("date", "==", note.date)
+        );
+      
+        const querySnapshot = await getDocs(noteQuery);
+        if (querySnapshot.size > 0) {
+          const noteDocRef = querySnapshot.docs[0].ref as DocumentReference<DocumentData>;
+          await deleteDoc(noteDocRef);
+        } else {
+          console.log("No matching document found.");
+        }
+      };
+      
+  
     return (
-        <div>
-            {/* NoteForm komponenten modtager onSubmit som en prop og sætter den til addNote funktionen */}
-            <NoteForm onSubmit={addNote} onExerciseChange={setSelectedExercise} />
-            {/* NoteList komponenten modtager notes som en prop */}
-            <NoteList notes={notes} />
-            <ProgressChart data={notes} selectedExercise={selectedExercise} />
-        </div>
+      <div>
+        <NoteForm onSubmit={addNote} onExerciseChange={setSelectedExercise} />
+        <NoteList notes={notes} onDelete={deleteNote} />
+        <ProgressChart data={notes} selectedExercise={selectedExercise} />
+      </div>
     );
-};
+  };
 
 
 export default NotePage;
